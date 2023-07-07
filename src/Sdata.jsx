4d871f642a32ca4d033bd7066243ae781dc010a7
Vimmi05 <vimmituli1999@gmail.com>
2023-07-07 14:39:40 +0530

    React - pictures/css/components/props

---
+++ src/Sdata.jsx
@@ -0,0 +1,33 @@
+const Sdata =[
+    {
+    sname:"DARK",
+    imgsrc:"https://picsum.photos/200/300",
+    title:"A Netflix original Series",
+    link:"#",
+    },
+    {
+    sname:"Money Heist",
+    imgsrc:"https://picsum.photos/201/300",
+    title:"A Netflix original Series",
+    link:"#",
+    },
+    {
+    sname:"Behind Her Eyes",
+    imgsrc:"https://picsum.photos/202/300",
+    title:"A Netflix original Series",
+    link:"#",
+    },
+    {
+    sname:"Never Have I Ever",
+    imgsrc:"https://picsum.photos/203/300",
+    title:"A Netflix original Series",
+    link:"#",
+    },
+    {
+    sname:"The Night Agent",
+    imgsrc:"https://picsum.photos/204/300",
+    title:"A Netflix original Series",
+    link:"#",
+    }
+]
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM  from "react-dom";
+import Card from "./Cards";
+import Sdata from "./Sdata";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+<>
+<h1 className="heading-style">List of 5 Netflix Series</h1>
+<Card className="card-style"
+sname={Sdata[0].sname}
+imgsrc={Sdata[0].imgsrc}
+title={Sdata[0].title}
+link={Sdata[0].link}
+/>
+
+<Card
+sname={Sdata[1].sname}
+imgsrc={Sdata[1].imgsrc}
+title={Sdata[1].title}
+link={Sdata[1].link}
+/>
+
+<Card
+sname={Sdata[2].sname}
+imgsrc={Sdata[2].imgsrc}
+title={Sdata[2].title}
+link={Sdata[2].link}
+/>
+
+<Card
+sname={Sdata[3].sname}
+imgsrc={Sdata[3].imgsrc}
+title={Sdata[3].title}
+link={Sdata[3].link}
+/>
+
+<Card
+sname={Sdata[4].sname}
+imgsrc={Sdata[4].imgsrc}
+title={Sdata[4].title}
+link={Sdata[4].link}
+/>
+</>,document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
