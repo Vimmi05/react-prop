534e07b2fdb828ef05384410b41bdaba7fe59df5
Vimmi05 <vimmituli1999@gmail.com>
2023-07-17 11:13:08 +0530

    july 17

--- src/index.js
+++ src/index.js
@@ -14,28 +14,28 @@ title={Sdata[0].title}
 link={Sdata[0].link}
 />
 
-<Card
+<Card className="card-style"
 sname={Sdata[1].sname}
 imgsrc={Sdata[1].imgsrc}
 title={Sdata[1].title}
 link={Sdata[1].link}
 />
 
-<Card
+<Card className="card-style"
 sname={Sdata[2].sname}
 imgsrc={Sdata[2].imgsrc}
 title={Sdata[2].title}
 link={Sdata[2].link}
 />
 
-<Card
+<Card className="card-style"
 sname={Sdata[3].sname}
 imgsrc={Sdata[3].imgsrc}
 title={Sdata[3].title}
 link={Sdata[3].link}
 />
 
-<Card
+<Card className="card-style"
 sname={Sdata[4].sname}
 imgsrc={Sdata[4].imgsrc}
 title={Sdata[4].title}
